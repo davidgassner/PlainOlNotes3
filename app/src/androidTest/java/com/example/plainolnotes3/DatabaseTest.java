51fb587ee9f9262c4253bb1e4aa74c2eb95d09fb
David Gassner <dgassnerdev@gmail.com>
2018-04-04 14:53:07 -0700

    03_05e

---
+++ app/src/androidTest/java/com/example/plainolnotes3/DatabaseTest.java
@@ -0,0 +1,58 @@
+package com.example.plainolnotes3;
+
+import android.arch.persistence.room.Room;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
+
+import com.example.plainolnotes3.database.AppDatabase;
+import com.example.plainolnotes3.database.NoteDao;
+import com.example.plainolnotes3.database.NoteEntity;
+import com.example.plainolnotes3.utilities.SampleData;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class DatabaseTest {
+    public static final String TAG = "Junit";
+    private AppDatabase mDb;
+    private NoteDao mDao;
+
+    @Before
+    public void createDb() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        mDb = Room.inMemoryDatabaseBuilder(context,
+                AppDatabase.class).build();
+        mDao = mDb.noteDao();
+        Log.i(TAG, "createDb");
+    }
+
+    @After
+    public void closeDb() {
+        mDb.close();
+        Log.i(TAG, "closeDb");
+    }
+
+    @Test
+    public void createAndRetrieveNotes() {
+        mDao.insertAll(SampleData.getNotes());
+        int count = mDao.getCount();
+        Log.i(TAG, "createAndRetrieveNotes: count=" + count);
+        assertEquals(SampleData.getNotes().size(), count);
+    }
+
+    @Test
+    public void compareStrings() {
+        mDao.insertAll(SampleData.getNotes());
+        NoteEntity original = SampleData.getNotes().get(0);
+        NoteEntity fromDb = mDao.getNoteById(1);
+        assertEquals(original.getText(), fromDb.getText());
+        assertEquals(1, fromDb.getId());
+    }
+}
--- app/src/main/java/com/example/plainolnotes3/utilities/SampleData.java
+++ app/src/main/java/com/example/plainolnotes3/utilities/SampleData.java
@@ -23,9 +23,9 @@ public class SampleData {
 
     public static List<NoteEntity> getNotes() {
         List<NoteEntity> notes = new ArrayList<>();
-        notes.add(new NoteEntity(1, getDate(0), SAMPLE_TEXT_1));
-        notes.add(new NoteEntity(2, getDate(-1), SAMPLE_TEXT_2));
-        notes.add(new NoteEntity(3, getDate(-2), SAMPLE_TEXT_3));
+        notes.add(new NoteEntity(getDate(0), SAMPLE_TEXT_1));
+        notes.add(new NoteEntity(getDate(-1), SAMPLE_TEXT_2));
+        notes.add(new NoteEntity(getDate(-2), SAMPLE_TEXT_3));
         return notes;
     }
 }
