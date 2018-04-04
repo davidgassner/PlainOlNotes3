6386ea71744a8d70491590f0765095072b751840
David Gassner <dgassnerdev@gmail.com>
2018-04-04 14:11:46 -0700

    03_03e

---
+++ app/src/main/java/com/example/plainolnotes3/database/AppDatabase.java
@@ -0,0 +1,28 @@
+package com.example.plainolnotes3.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {NoteEntity.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public static final String DATABASE_NAME = "AppDatabase.db";
+    private static volatile AppDatabase instance;
+    private static final Object LOCK = new Object();
+
+    public abstract NoteDao noteDao();
+
+    public static AppDatabase getInstance(Context context) {
+        if (instance == null) {
+            synchronized (LOCK) {
+                if (instance == null) {
+                    instance = Room.databaseBuilder(context.getApplicationContext(),
+                            AppDatabase.class, DATABASE_NAME).build();
+                }
+            }
+        }
+
+        return instance;
+    }
+}
