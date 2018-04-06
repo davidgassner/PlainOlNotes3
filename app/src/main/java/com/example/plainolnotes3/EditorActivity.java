6f2b40f426dce9394a2d47dc8d24d1cf5c37ed26
David Gassner <dgassnerdev@gmail.com>
2018-04-06 16:11:14 -0700

    06_02e

--- app/src/main/java/com/example/plainolnotes3/EditorActivity.java
+++ app/src/main/java/com/example/plainolnotes3/EditorActivity.java
@@ -4,18 +4,14 @@ import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 
 import com.example.plainolnotes3.database.NoteEntity;
-import com.example.plainolnotes3.utilities.Constants;
 import com.example.plainolnotes3.viewmodel.EditorViewModel;
 
 import butterknife.BindView;
--- app/src/main/java/com/example/plainolnotes3/MainActivity.java
+++ app/src/main/java/com/example/plainolnotes3/MainActivity.java
@@ -6,10 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -82,6 +82,10 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setHasFixedSize(true);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(layoutManager);
+
+        DividerItemDecoration divider = new DividerItemDecoration(
+                mRecyclerView.getContext(), layoutManager.getOrientation());
+        mRecyclerView.addItemDecoration(divider);
 
     }
 
